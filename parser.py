04756fcbbc45f2aa00539f83501fb14e210d3c40
jiwen xin <kevinxin@scripps.edu>
2020-11-18 11:21:40 -0800

    initial commit

---
+++ parser.py
@@ -0,0 +1,39 @@
+import csv
+import os
+import json
+
+
+def load_data(data_folder):
+    edges_file_path = os.path.join(data_folder, "wellness_kg_edges.tsv")
+    nodes_file_path = os.path.join(data_folder, "wellness_kg_nodes.tsv")
+    nodes_f = open(nodes_file_path)
+    edges_f = open(edges_file_path)
+    nodes_data = csv.reader(nodes_f, delimiter="\t")
+    edges_data = csv.reader(edges_f, delimiter="\t")
+    next(nodes_data)
+    id_type_mapping = {}
+    for line in nodes_data:
+        id_type_mapping[line[0]] = line[2].split(':')[-1] if line[2].startswith("biolink:") else line[2]
+    next(edges_data)
+    for line in edges_data:
+        yield {
+            "subject": {
+                "id": line[0],
+                line[0].split(':')[0]: line[0],
+                "name": line[-2],
+                "type": id_type_mapping[line[0]]
+            },
+            "association": {
+                "edge_label": line[1].split(':')[-1],
+                "relation": line[3],
+                "N": float(line[4]),
+                "rho": float(line[5]),
+                "pval": float(line[6])
+            },
+            "object": {
+                "id": line[2],
+                line[2].split(':')[0]: line[2],
+                "name": line[-1],
+                "type": id_type_mapping[line[2]]
+            }
+        }
