67ea97ca90bfc6ef698c08c0abd72be585773cbc
rtroper <rtroper4@gmail.com>
2021-09-01 11:08:00 -0700

    Update file parser for v1.5 knowledge graph files

--- file_parser.py
+++ file_parser.py
@@ -1,11 +1,10 @@
 import csv
 import os
-import json
 
 
 def load_data(data_folder):
-    edges_file_path = os.path.join(data_folder, "wellness_kg_edges_v1.3.tsv")
-    nodes_file_path = os.path.join(data_folder, "wellness_kg_nodes_v1.3.tsv")
+    edges_file_path = os.path.join(data_folder, "wellness_kg_edges_v1.5.tsv")
+    nodes_file_path = os.path.join(data_folder, "wellness_kg_nodes_v1.5.tsv")
     nodes_f = open(nodes_file_path)
     edges_f = open(edges_file_path)
     nodes_data = csv.reader(nodes_f, delimiter="\t")
@@ -13,9 +12,12 @@ def load_data(data_folder):
     next(nodes_data)
     id_name_mapping = {}
     id_type_mapping = {}
+
     for line in nodes_data:
         id_name_mapping[line[0]] = line[1]
-        id_type_mapping[line[0]] = line[2].split(':')[-1] if line[2].startswith("biolink:") else line[2]
+        id_type_mapping[line[0]] = line[2].split(
+            ':')[-1] if line[2].startswith("biolink:") else line[2]
+
     next(edges_data)
     for line in edges_data:
         if line[0] and line[1] and line[0].split(':')[0] and line[2].split(':')[0]:
@@ -39,18 +41,34 @@ def load_data(data_folder):
             predicate = {
                 "type": line[1].split(':')[-1] if line[1].startswith("biolink:") else line[1],
                 "relation": line[3],
-                "category": line[9].split(':')[-1] if line[9].startswith("biolink:") else line[9],
+                "category": line[6].split(':')[-1] if line[6].startswith("biolink:") else line[6],
                 "provided_by": "Multiomics Provider: Wellness",
                 "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/Wellness-KP",
-                "N": int(float(line[4])),
-                "rho": int(float(line[5])*1e5)/1e5,
-                "Bonferroni_pval": float(line[6])
+                "N": int(float(line[7])),
+                "type_of_relationship": line[9],
+                "strength_of_relationship": f'{float(line[10]):.4f}',
+                "bonferroni_pval": f'{float(line[13]):.4e}'
             }
 
+            # Add weighted pvalue, if available
+            weighted_pvalue = None if (
+                line[8] == '') else f'{float(line[8]):.4e}'
+            if not(weighted_pvalue is None):
+                predicate.update({"weighted_pvalue": weighted_pvalue})
+
+            # Add qualifier, if available
+            qualifier = None if (line[11] == '') else line[11]
+            qualifier_value = None if (line[12] == '') else line[12]
+            if not(qualifier is None):
+                predicate.update(
+                    {'qualifier': qualifier, 'qualifier_value': qualifier_value})
+
             # Yield subject, predicate, and object properties
-            yield {
-                "_id": '-'.join([line[0], line[1], line[2], str(int(float(line[4]))), str(line[5])[:6], str(line[6])]),
+            data = {
+                "_id": '-'.join([line[0], line[1], line[2], str(int(float(line[7]))), str(line[10])[:6], str(line[13])]),
                 "subject": subject,
                 "predicate": predicate,
                 "object": object_
             }
+
+            yield data
