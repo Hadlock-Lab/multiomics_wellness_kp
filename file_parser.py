7a9df5591e23bf3006b9c8a404af26019db91a0c
Colleen Xu <colleenhxu@gmail.com>
2021-09-22 17:25:47 -0700

    remove rounding on numeric fields

--- file_parser.py
+++ file_parser.py
@@ -46,13 +46,13 @@ def load_data(data_folder):
                 "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/Wellness-KP",
                 "N": int(float(line[7])),
                 "type_of_relationship": line[9],
-                "strength_of_relationship": round(float(line[10]),4),
-                "bonferroni_pval": round(float(line[13]),4)
+                "strength_of_relationship": float(line[10]),
+                "bonferroni_pval": float(line[13])
             }
 
             # Add weighted pvalue, if available
             weighted_pvalue = None if (
-                line[8] == '') else round(float(line[8]),4)
+                line[8] == '') else float(line[8])
             if not(weighted_pvalue is None):
                 predicate.update({"weighted_pvalue": weighted_pvalue})
 
