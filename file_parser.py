773165a79c54666338c95f8af612acc52ebfcbeb
Eric Deutsch <edeutsch@systemsbiology.org>
2021-12-18 09:19:55 +0000

    Update parser TRAPI output; update version and manifest to 1.6

--- file_parser.py
+++ file_parser.py
@@ -1,10 +1,31 @@
 import csv
 import os
+import json
+
+attribute_source = "infores:biothings-multiomics-wellness"
+
+correlation_statistic = {
+    "Ridge regression coefficient": {
+        "attribute_source": attribute_source,
+        "attribute_type_id": "NCIT:C53237", # Regression Method -- http://purl.obolibrary.org/obo/NCIT_C53237
+        "description": "Ridge regression coefficient was used to compute the value for the association",
+        "value": "ENM:8000094",
+        "value_type_id": "biolink:id"
+    },
+    "Spearman Correlation": {
+        "attribute_source": attribute_source,
+        "attribute_type_id": "NCIT:C53236", # Correlation Test -- http://purl.obolibrary.org/obo/NCIT_C53236
+        "description": "Spearman Correlation Test was used to compute the p-value for the association",
+        "value": "NCIT:C53249", # Spearman Correlation Test -- http://purl.obolibrary.org/obo/NCIT_C53249
+        "value_type_id": "biolink:id"
+    }
+}
+
 
 
 def load_data(data_folder):
-    edges_file_path = os.path.join(data_folder, "wellness_kg_edges_v1.5.1.tsv")
-    nodes_file_path = os.path.join(data_folder, "wellness_kg_nodes_v1.5.1.tsv")
+    edges_file_path = os.path.join(data_folder, "wellness_kg_edges_v1.6.tsv")
+    nodes_file_path = os.path.join(data_folder, "wellness_kg_nodes_v1.6.tsv")
     nodes_f = open(nodes_file_path)
     edges_f = open(edges_file_path)
     nodes_data = csv.reader(nodes_f, delimiter="\t")
@@ -15,68 +36,125 @@ def load_data(data_folder):
 
     for line in nodes_data:
         id_name_mapping[line[0]] = line[1]
-        id_type_mapping[line[0]] = line[2].split(
-            ':')[-1] if line[2].startswith("biolink:") else line[2]
+        id_type_mapping[line[0]] = line[2]
 
     next(edges_data)
     for line in edges_data:
         if line[0] and line[1] and line[0].split(':')[0] and line[2].split(':')[0]:
-            # Specify properties for subject
+
+            prefix = line[0].split(':')[0]
             subject = {
                 "id": line[0],
+                prefix: line[0],
                 "name": id_name_mapping[line[0]],
                 "type": id_type_mapping[line[0]]
             }
 
-            line0_prefix, line0_id = line[0].split(":")
-            subject_specialfield = line[0] if (
-                line0_prefix=="CHEBI") else line0_id
-            subject.update( { line0_prefix.replace('.', '_'): subject_specialfield })
-
-            # Specify properties for object
+            prefix = line[2].split(':')[0]
             object_ = {
                 "id": line[2],
+                prefix: line[2],
                 "name": id_name_mapping[line[2]],
                 "type": id_type_mapping[line[2]]
             }
 
-            line2_prefix, line2_id = line[2].split(":")
-            object_specialfield = line[2] if (
-                line2_prefix=="CHEBI") else line2_id
-            object_.update( { line2_prefix.replace('.', '_'): object_specialfield })
+            # properties for predicate/association
+            edge_attributes = []
 
-            # Specify properties for predicate
-            predicate = {
-                "type": line[1].split(':')[-1] if line[1].startswith("biolink:") else line[1],
-                "relation": line[3],
-                "category": line[6].split(':')[-1] if line[6].startswith("biolink:") else line[6],
-                "provided_by": "Multiomics Provider: Wellness",
-                "provenance": "https://github.com/NCATSTranslator/Translator-All/wiki/Wellness-KP",
-                "N": int(float(line[7])),
-                "type_of_relationship": line[9],
-                "strength_of_relationship": float(line[10]),
-                "bonferroni_pval": float(line[13])
-            }
+            # Type_of_relationship
+            # could be Ridge regression or Spearman
+            if line[8] in correlation_statistic:
+                edge_attributes.append(correlation_statistic[line[8]])
+            else:
+                raise Exception(f"Column 8 has unexpected value {line[8]}")
 
-            # Add weighted pvalue, if available
-            weighted_pvalue = None if (
-                line[8] == '') else float(line[8])
-            if not(weighted_pvalue is None):
-                predicate.update({"weighted_pvalue": weighted_pvalue})
+            # strength_of_relationship
+            edge_attributes.append(
+                {
+                    "attribute_source": attribute_source,
+                    "attribute_type_id": "CURIE:strength_of_relationship", # ???
+                    "description": "Somehow related to Type_of_relationship", # ???
+                    "value": float(line[9]),
+                    "value_type_id": "biolink:XXX" # ???
+                }
+            )
+
+            # relation
+            edge_attributes.append(
+                {
+                    "attribute_source": attribute_source,
+                    "attribute_type_id": line[6], # ???
+                    "description": "Predicate id?", # ???
+                    "value": line[3],
+                    "value_type_id": "biolink:id" # ???
+                }
+            )
+
+            # N
+            edge_attributes.append(
+                {
+                    "attribute_source": attribute_source,
+                    "attribute_type_id": "GECKO:0000106", # ??? sample size - http://purl.obolibrary.org/obo/GECKO_0000106
+                    "description": "Sample size used to compute the correlation", # ???
+                    "value": int(float(line[7]))
+                }
+            )
+
+            # bonferroni_pval
+            edge_attributes.append(
+                {
+                    "attribute_source": attribute_source,
+                    "attribute_type_id": "NCIT:C61594", # ???
+                    "description": "Bonferroni pval XXX", # ???
+                    "value": float(line[12])
+                }
+            )
+
 
             # Add qualifier, if available
-            qualifier = None if (line[11] == '') else line[11]
-            qualifier_value = None if (line[12] == '') else line[12]
+            qualifier = None if (line[10] == '' or line[10] == 'nan') else line[10]
+            qualifier_value = None if (line[11] == '' or line[11] == 'nan') else line[11]
             if not(qualifier is None):
-                predicate.update(
-                    {'qualifier': qualifier, 'qualifier_value': qualifier_value})
+                edge_attributes.append(
+                    {
+                        "attribute_source": attribute_source,
+                        "attribute_type_id": qualifier, # ???
+                        "description": "qualifier XXX", # ???
+                        "value": qualifier_value,
+                        "value_type_id": "XXX ???" # ??? 
+                    }
+                )
+
+
+
+            association = {
+                "edge_label": line[1],
+                "edge_attributes": edge_attributes
+            }
 
             # Yield subject, predicate, and object properties
             data = {
-                "_id": '-'.join([line[0], line[1], line[2], str(int(float(line[7]))), str(line[10])[:6], str(line[13])]),
+                "_id": '-'.join(['WKP',line[0], line[1], line[2], str(int(float(line[7]))), str(line[9])[:6], str(line[12])]),
                 "subject": subject,
-                "predicate": predicate,
+                "association": association,
                 "object": object_
             }
-
             yield data
+
+        else:
+            print(f"Cannot find prefix for {line} !")
+
+
+
+def main():
+    write_top_ten = True
+    if (write_top_ten):
+        gen = load_data('test')
+        for x in range(0, 10):
+            print(json.dumps(next(gen), sort_keys=True, indent=2))
+    else:
+        [_ for _ in load_data('test')]
+
+
+if __name__ == '__main__':
+    main()
--- version.py
+++ version.py
@@ -2,4 +2,4 @@
 def get_release(self):
     # "self" is a dumper instance, see:
     # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
-    return "1.5.1"
+    return "1.6"
