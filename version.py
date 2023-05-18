e1a5b336349a0d090fdff30868dd42540de3798b
GG <gglusman@isbscience.org>
2023-05-18 13:55:45 -0700

    Update version.py for v1.7

--- version.py
+++ version.py
@@ -2,4 +2,4 @@
 def get_release(self):
     # "self" is a dumper instance, see:
     # https://github.com/biothings/biothings.api/blob/master/biothings/hub/dataload/dumper.py
-    return "1.6"
+    return "1.7"
